9a15b59facb35d23ffbc2cc7869026a659505d80
Nauval Rizky <enuma.alrizky@gmail.com>
2022-10-30 13:10:05 +0700

    update compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,7 @@ android {
 
 dependencies {
     // Android
-    implementation("androidx.activity:activity-compose:1.6.0")
+    implementation("androidx.activity:activity-compose:1.6.1")
     implementation("androidx.compose.material:material-icons-extended:1.4.0-alpha01")
     implementation("androidx.compose.material3:material3:1.1.0-alpha01")
     implementation("androidx.compose.ui:ui:1.4.0-alpha01")
@@ -97,7 +97,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-viewbinding:1.4.0-alpha01")
     implementation("androidx.datastore:datastore:1.0.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
-    implementation("androidx.navigation:navigation-compose:2.5.2")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.work:work-runtime-ktx:2.7.1")
     implementation("com.google.accompanist:accompanist-flowlayout:0.26.5-rc")
     implementation("com.google.accompanist:accompanist-navigation-material:0.26.5-rc")
