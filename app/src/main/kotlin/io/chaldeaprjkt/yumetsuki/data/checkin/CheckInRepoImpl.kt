9294c9abdbff0c3376e3cd24b453da9e9800506c
Nauval Rizky <enuma.alrizky@gmail.com>
2022-11-01 23:39:15 +0700

    insert stub record when not yet signed or already signed

--- app/src/main/kotlin/io/chaldeaprjkt/yumetsuki/data/checkin/CheckInRepoImpl.kt
+++ app/src/main/kotlin/io/chaldeaprjkt/yumetsuki/data/checkin/CheckInRepoImpl.kt
@@ -3,14 +3,15 @@ package io.chaldeaprjkt.yumetsuki.data.checkin
 import io.chaldeaprjkt.yumetsuki.data.checkin.entity.CheckInNote
 import io.chaldeaprjkt.yumetsuki.data.checkin.source.CheckInNetworkSource
 import io.chaldeaprjkt.yumetsuki.data.checkin.source.CheckInNoteDao
+import io.chaldeaprjkt.yumetsuki.data.common.HoYoApiCode
 import io.chaldeaprjkt.yumetsuki.data.common.HoYoData
+import io.chaldeaprjkt.yumetsuki.data.common.HoYoError
 import io.chaldeaprjkt.yumetsuki.data.gameaccount.entity.GameAccount
 import io.chaldeaprjkt.yumetsuki.data.user.entity.User
 import io.chaldeaprjkt.yumetsuki.domain.repository.CheckInRepo
 import kotlinx.coroutines.flow.onEach
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -21,12 +22,15 @@ class CheckInRepoImpl @Inject constructor(
 ) : CheckInRepo {
 
     override val data = checkInNoteDao.all()
+    private val dateFmt = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
     override suspend fun sync(user: User, gameAccount: GameAccount) =
         checkInNetworkSource.status(cookie = user.cookie, game = gameAccount.game)
             .onEach {
-                if (it is HoYoData && it.data.signed) {
-                    CheckInNote(gameAccount.uid, user.uid, gameAccount.game, it.data.today)
+                if (it is HoYoData) {
+                    val yesterday = LocalDate.now().minusDays(1).format(dateFmt)
+                    val date = if (it.data.signed) it.data.today else yesterday
+                    CheckInNote(gameAccount.uid, user.uid, gameAccount.game, date)
                         .let { note -> checkInNoteDao.insert(note) }
                 }
             }
@@ -34,9 +38,11 @@ class CheckInRepoImpl @Inject constructor(
     override suspend fun checkIn(user: User, gameAccount: GameAccount) =
         checkInNetworkSource.checkIn(cookie = user.cookie, game = gameAccount.game)
             .onEach {
-                if (it is HoYoData) {
-                    val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-                    val today = formatter.format(Calendar.getInstance().time)
+                val signed = it is HoYoError.Api && (it.code == HoYoApiCode.CheckedIntoHoyolab ||
+                        it.code == HoYoApiCode.ClaimedDailyReward)
+
+                if (it is HoYoData || signed) {
+                    val today = LocalDate.now().format(dateFmt)
                     CheckInNote(gameAccount.uid, user.uid, gameAccount.game, today)
                         .let { note -> checkInNoteDao.insert(note) }
                 }
