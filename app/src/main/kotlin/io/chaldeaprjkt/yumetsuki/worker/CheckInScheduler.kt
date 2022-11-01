0808a22d52576a3e403c76188d6f7515d1289c39
Nauval Rizky <enuma.alrizky@gmail.com>
2022-11-02 03:26:24 +0700

    use java.time.Instant for simpler next server refresh calculation

--- app/src/main/kotlin/io/chaldeaprjkt/yumetsuki/worker/CheckInScheduler.kt
+++ app/src/main/kotlin/io/chaldeaprjkt/yumetsuki/worker/CheckInScheduler.kt
@@ -10,11 +10,13 @@ import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import io.chaldeaprjkt.yumetsuki.data.gameaccount.entity.HoYoGame
-import io.chaldeaprjkt.yumetsuki.util.CommonFunction
 import io.chaldeaprjkt.yumetsuki.util.extension.workManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.util.Calendar
+import java.time.Duration
+import java.time.Instant
+import java.time.ZoneId
+import java.time.temporal.ChronoUnit
 import java.util.concurrent.TimeUnit
 
 @HiltWorker
@@ -30,10 +32,18 @@ class CheckInScheduler @AssistedInject constructor(
             return HoYoGame.Adapter.fromJson(gameId)
         }
 
+    private fun nextCheckIn(): Duration {
+        val now = Instant.now()
+        val serverZone = ZoneId.of("UTC+8")
+        val next = now.plus(1, ChronoUnit.DAYS).atZone(serverZone).withHour(0)
+            .withMinute((10..30).random()).toInstant()
+
+        return Duration.between(now, next)
+    }
+
     override suspend fun doWork(): Result {
         return withContext(Dispatchers.IO) {
-            val time = CommonFunction.getTimeLeftUntilChinaTime(true, 0, Calendar.getInstance())
-            CheckInWorker.start(workManager, game, time)
+            CheckInWorker.start(workManager, game, nextCheckIn().toMinutes())
             Result.success()
         }
     }
@@ -45,15 +55,12 @@ class CheckInScheduler @AssistedInject constructor(
 
         fun post(workManager: WorkManager?, game: HoYoGame) {
             workManager?.cancelAllWorkByTag(workerTag(game))
-            val workRequest = OneTimeWorkRequestBuilder<CheckInScheduler>()
-                .setInitialDelay(1, TimeUnit.MINUTES)
-                .addTag(workerTag(game))
-                .build()
+            val workRequest =
+                OneTimeWorkRequestBuilder<CheckInScheduler>().setInitialDelay(1, TimeUnit.MINUTES)
+                    .addTag(workerTag(game)).build()
 
             workManager?.enqueueUniqueWork(
-                workerTag(game),
-                ExistingWorkPolicy.REPLACE,
-                workRequest
+                workerTag(game), ExistingWorkPolicy.REPLACE, workRequest
             )
         }
     }
