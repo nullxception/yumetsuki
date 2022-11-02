45d4ac8825846c3e5c84ba6c9396bac28706d489
Nauval Rizky <enuma.alrizky@gmail.com>
2022-11-02 16:46:41 +0700

    Yumetsuki v1.2
    - Fix check-in scheduler keeps triggering right after execution completed
    - Fix Widget did not loaded on Android 10 and 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         minSdk = 29
         @SuppressLint("OldTargetApi") // TODO: Fix WallpaperManager usage on targetSdk 33
         targetSdk = 32
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
         vectorDrawables {
             useSupportLibrary = true
         }
