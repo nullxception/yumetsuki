43ea2ddc57f76776437d19541fbd7c9b9f8beb05
Nauval Rizky <enuma.alrizky@gmail.com>
2023-02-28 06:43:36 +0700

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,56 +107,61 @@ android {
 }
 
 dependencies {
+    val versionCompose = "1.4.0-beta02"
+    val versionAccompanist = "0.29.1-alpha"
+    val versionHilt = "2.45"
+    val versionRoom = "2.5.0"
+    val versionMoshi = "1.14.0"
+
     // Android
     implementation("androidx.core:core-splashscreen:1.0.0")
     implementation("androidx.activity:activity-compose:1.6.1")
-    implementation("androidx.compose.material:material-icons-extended:1.4.0-alpha01")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha01")
-    implementation("androidx.compose.ui:ui:1.4.0-alpha01")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.0-alpha01")
-    implementation("androidx.compose.ui:ui-viewbinding:1.4.0-alpha01")
+    implementation("androidx.compose.material:material-icons-extended:$versionCompose")
+    implementation("androidx.compose.material3:material3:1.1.0-alpha07")
+    implementation("androidx.compose.ui:ui:$versionCompose")
+    implementation("androidx.compose.ui:ui-tooling-preview:$versionCompose")
+    implementation("androidx.compose.ui:ui-viewbinding:$versionCompose")
     implementation("androidx.datastore:datastore:1.0.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
     implementation("androidx.navigation:navigation-compose:2.5.3")
-    implementation("androidx.work:work-runtime-ktx:2.7.1")
-    implementation("com.google.accompanist:accompanist-flowlayout:0.27.0")
-    implementation("com.google.accompanist:accompanist-navigation-material:0.27.0")
-    implementation("com.google.accompanist:accompanist-pager-indicators:0.27.0")
-    implementation("com.google.accompanist:accompanist-pager:0.27.0")
-    implementation("com.google.accompanist:accompanist-permissions:0.27.0")
-    implementation("com.google.accompanist:accompanist-navigation-animation:0.27.0")
+    implementation("androidx.work:work-runtime-ktx:2.8.0")
+    implementation("com.google.accompanist:accompanist-flowlayout:$versionAccompanist")
+    implementation("com.google.accompanist:accompanist-navigation-material:$versionAccompanist")
+    implementation("com.google.accompanist:accompanist-pager-indicators:$versionAccompanist")
+    implementation("com.google.accompanist:accompanist-pager:$versionAccompanist")
+    implementation("com.google.accompanist:accompanist-permissions:$versionAccompanist")
+    implementation("com.google.accompanist:accompanist-navigation-animation:$versionAccompanist")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.4.0-alpha01")
+    debugImplementation("androidx.compose.ui:ui-tooling:$versionCompose")
 
     // Dependency Injection
-    implementation("com.google.dagger:hilt-android:2.44")
+    implementation("com.google.dagger:hilt-android:$versionHilt")
     implementation("androidx.hilt:hilt-work:1.0.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:$versionHilt")
     kapt("androidx.hilt:hilt-compiler:1.0.0")
 
     // Networking
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.10")
-    implementation("com.github.skydoves:sandwich:1.3.2")
+    implementation("com.github.skydoves:sandwich:1.3.3")
 
     // Storage
-    val versionRoom = "2.4.3"
     implementation("androidx.room:room-runtime:$versionRoom")
     implementation("androidx.room:room-ktx:$versionRoom")
     ksp("androidx.room:room-compiler:$versionRoom")
 
     // Parser
-    implementation("com.squareup.moshi:moshi:1.14.0")
+    implementation("com.squareup.moshi:moshi:$versionMoshi")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.14.0")
+    ksp("com.squareup.moshi:moshi-kotlin-codegen:$versionMoshi")
 
     // Assets
     implementation("io.coil-kt:coil-compose:2.2.2")
     implementation("br.com.devsrsouza.compose.icons.android:feather:1.0.0")
 
     // Licenses
-    implementation("com.mikepenz:aboutlibraries-core:10.5.1")
+    implementation("com.mikepenz:aboutlibraries-core:10.6.1")
 }
 
 tasks {
