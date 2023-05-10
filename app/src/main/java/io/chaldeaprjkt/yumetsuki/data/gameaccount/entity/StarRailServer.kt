0bb9fb3d8fd8370df44355aed4645b420130c456
Nauval Rizky <enuma.alrizky@gmail.com>
2023-05-10 08:37:43 +0700

    fix(StarRailServer): parse the rest

--- app/src/main/java/io/chaldeaprjkt/yumetsuki/data/gameaccount/entity/StarRailServer.kt
+++ app/src/main/java/io/chaldeaprjkt/yumetsuki/data/gameaccount/entity/StarRailServer.kt
@@ -1,7 +1,10 @@
 package io.chaldeaprjkt.yumetsuki.data.gameaccount.entity
 
 enum class StarRailServer(override val regionId: String) : GameServer {
-    ASIA("prod_official_asia");
+    ASIA("prod_official_asia"),
+    EU("prod_official_euro"),
+    TW("prod_official_cht"),
+    USA("prod_official_usa");
 
     companion object {
         fun fromRegionId(value: String): StarRailServer {
