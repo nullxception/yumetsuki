2d456e35e162a43320c4ffa43ac7b2983fe4bc6d
Nauval Rizky <enuma.alrizky@gmail.com>
2023-06-07 19:55:46 +0700

    update custom NavAnim

--- app/src/main/java/io/chaldeaprjkt/yumetsuki/ui/navigation/NavAnim.kt
+++ app/src/main/java/io/chaldeaprjkt/yumetsuki/ui/navigation/NavAnim.kt
@@ -1,53 +1,54 @@
 package io.chaldeaprjkt.yumetsuki.ui.navigation
 
 import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.FiniteAnimationSpec
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.ui.unit.IntOffset
 
-fun <S> AnimatedContentScope<S>.fadeSlideInDown(
+fun <S> AnimatedContentTransitionScope<S>.fadeSlideInDown(
     animationSpec: FiniteAnimationSpec<IntOffset> = tween(250),
     initialOffset: (Int) -> Int = { -100 }
 ) = fadeIn() + slideIntoContainer(
-    towards = AnimatedContentScope.SlideDirection.Down,
+    towards = AnimatedContentTransitionScope.SlideDirection.Down,
     animationSpec = animationSpec,
     initialOffset = initialOffset
 )
 
-fun <S> AnimatedContentScope<S>.fadeSlideInRight(
+fun <S> AnimatedContentTransitionScope<S>.fadeSlideInRight(
     animationSpec: FiniteAnimationSpec<IntOffset> = tween(250),
     initialOffset: (Int) -> Int = { it / 2 }
 ) = fadeIn() + slideIntoContainer(
-    towards = AnimatedContentScope.SlideDirection.Right,
+    towards = AnimatedContentTransitionScope.SlideDirection.Right,
     animationSpec = animationSpec,
     initialOffset = initialOffset
 )
 
-fun <S> AnimatedContentScope<S>.fadeSlideInLeft(
+fun <S> AnimatedContentTransitionScope<S>.fadeSlideInLeft(
     animationSpec: FiniteAnimationSpec<IntOffset> = tween(250),
     initialOffset: (Int) -> Int = { it / 2 }
 ) = fadeIn() + slideIntoContainer(
-    towards = AnimatedContentScope.SlideDirection.Left,
+    towards = AnimatedContentTransitionScope.SlideDirection.Left,
     animationSpec = animationSpec,
     initialOffset = initialOffset
 )
 
-fun <S> AnimatedContentScope<S>.fadeSlideOutLeft(
+fun <S> AnimatedContentTransitionScope<S>.fadeSlideOutLeft(
     animationSpec: FiniteAnimationSpec<IntOffset> = tween(250),
     targetOffset: (Int) -> Int = { it / 2 }
 ) = fadeOut() + slideOutOfContainer(
-    towards = AnimatedContentScope.SlideDirection.Left,
+    towards = AnimatedContentTransitionScope.SlideDirection.Left,
     animationSpec = animationSpec,
     targetOffset = targetOffset
 )
 
-fun <S> AnimatedContentScope<S>.fadeSlideOutRight(
+fun <S> AnimatedContentTransitionScope<S>.fadeSlideOutRight(
     animationSpec: FiniteAnimationSpec<IntOffset> = tween(250),
     targetOffset: (Int) -> Int = { it / 2 }
 ) = fadeOut() + slideOutOfContainer(
-    towards = AnimatedContentScope.SlideDirection.Right,
+    towards = AnimatedContentTransitionScope.SlideDirection.Right,
     animationSpec = animationSpec,
     targetOffset = targetOffset
 )
