c3ba8c966c6f110d30728d10af9338586d5ff81d
Nauval Rizky <enuma.alrizky@gmail.com>
2023-08-07 08:09:53 +0700

    HoYoCookie: add support for Cookie V2

--- app/src/main/java/io/chaldeaprjkt/yumetsuki/domain/common/HoYoCookie.kt
+++ app/src/main/java/io/chaldeaprjkt/yumetsuki/domain/common/HoYoCookie.kt
@@ -14,16 +14,20 @@ data class HoYoCookie(val text: String) {
         get() = map[Lang] ?: DefaultLang
 
     val uid
-        get() = map[UID]?.toIntOrNull() ?: 0
+        get() = (map[UIDV2] ?: map[UID])?.toIntOrNull() ?: 0
 
     override fun toString() = text.split("\n").joinToString("") { it.trim() }.trimQuotes()
 
-    fun isValid() = map.containsKey(Lang) && map.containsKey(Token) && map.containsKey(UID)
+    private fun hasToken() = map.containsKey(Token) || map.containsKey(TokenV2)
+    private fun hasUID() = map.containsKey(UIDV2) || map.containsKey(UIDV2)
+    fun isValid() = map.containsKey(Lang) && hasToken() && hasUID()
 
     companion object {
         const val Lang = "mi18nLang"
         const val DefaultLang = "en-us"
         const val Token = "ltoken"
         const val UID = "ltuid"
+        const val TokenV2 = "ltoken_v2"
+        const val UIDV2 = "ltuid_v2"
     }
 }
