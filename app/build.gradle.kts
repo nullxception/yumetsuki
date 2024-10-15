a36a22281ab16d7902a839cbda0071c5eafe2061
Nauval Rizky <enuma.alrizky@gmail.com>
2024-10-15 19:48:12 +0700

    Yumetsuki v2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         minSdk = 29
         //noinspection ExpiredTargetSdkVersion
         targetSdk = 32 // use 32 since permission handling somehow isn't working well
-        versionCode = 25
-        versionName = "2.5"
+        versionCode = 26
+        versionName = "2.6"
         vectorDrawables { useSupportLibrary = true }
 
         ksp { arg("room.schemaLocation", "$projectDir/schemas") }
